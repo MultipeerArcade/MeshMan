bca07625ec09dc1ca96d7b270f94bc237ae5097b
Russell Pecka <rpecka98@gmail.com>
2019-12-25 10:09:16 -0600

    Properly fixed dark mode issues and changed the gallows images to templates

---
+++ MeshMan/Assets.swift
@@ -0,0 +1,16 @@
+//
+//  Assets.swift
+//  MeshMan
+//
+//  Created by Russell Pecka on 12/25/19.
+//  Copyright © 2019 Russell Pecka. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum Assets {
+    
+    static let gallowsColor = UIColor(named: "Gallows Color")!
+    
+}
