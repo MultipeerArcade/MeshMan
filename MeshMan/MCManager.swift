4ae41b9477a59930870d72621a280307c1975260
Russell Pecka <rpecka98@gmail.com>
2019-12-30 00:17:05 -0600

    Added logic to disconnect when backgrounding the app

--- MeshMan/MCManager.swift
+++ MeshMan/MCManager.swift
@@ -92,7 +92,16 @@ class MCManager: NSObject, MCSessionDelegate, MCNearbyServiceAdvertiserDelegate,
         self.turnHelper = TurnManager(session: session, myPeerID: peerID)
         super.init()
         session.delegate = self
+        setUpBackgroundMonitoring()
 	}
+    
+    private func setUpBackgroundMonitoring() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleBackgrounding(notification:)), name: UIApplication.willResignActiveNotification, object: nil)
+    }
+    
+    @objc private func handleBackgrounding(notification: NSNotification) {
+        session.disconnect()
+    }
 	
 	private func makeBrowser() -> MCNearbyServiceBrowser {
 		return MCNearbyServiceBrowser(peer: self.peerID, serviceType: MCManager.serviceType)
